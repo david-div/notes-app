d6d7754b9c7a178783c9923c11b16f72b26be933
David Divilly <d.divilly@gmail.com>
2018-01-06 23:00:17 +0000

    Updated testing to show func name

--- spec/spec-helper.js
+++ spec/spec-helper.js
@@ -1,16 +1,25 @@
+
+
 var assert = {
   isTrue: function(assertionToCheck){
     if (!assertionToCheck) {
       throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
     } else {
-      console.log("Assertion passed: " + assertionToCheck)
+      var funcName = arguments.callee.caller.toString().split('\n')[0]
+      console.log("Assertion passed: " + funcName)
     }
   },
   isEqualTo: function(firstParam, secondParm){
     if (firstParam !== secondParm){
       throw new Error("Assertion failed: " + " is not truthy");
     } else {
-      console.log("Assertion passed: ")
+      var funcName = arguments.callee.caller.toString().split('\n')[0]
+      console.log("Assertion passed: " + funcName)
     }
   }
 };
+
+// function describe (test){
+//   console.log(test.name + ": ")
+//   test();
+// }
